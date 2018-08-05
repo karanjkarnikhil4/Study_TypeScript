9c61ce9ab738d9d060ec57960c80c3019bc1e5a5
Nikhil <karanjkarnikhil4@gmail.com>
2018-08-05 11:59:23 +0530

    adding modified files

--- FirstProject/NameSpaces/Namespaces.js
+++ FirstProject/NameSpaces/Namespaces.js
@@ -1,24 +1,4 @@
-//namespace can exist in more than one file for eg here same namespace that is Math is in Circle.ts and Rectangle.ts
-// we need to export the functions or variables to use them outside the namespace. else they cannot be used outside the scope
-// suppose we want to use PI variable originally present in Circle.ts in another file that is Rectangle.ts . although they are in same namespace still it is not possible without exporting them.
-var MyMath;
-(function (MyMath) {
-    var MyCircle;
-    (function (MyCircle) {
-        MyCircle.PI = 3.14;
-        function CalculateCircumference(diameter) {
-            return MyCircle.PI * diameter;
-        }
-        MyCircle.CalculateCircumference = CalculateCircumference;
-    })(MyCircle = MyMath.MyCircle || (MyMath.MyCircle = {}));
-})(MyMath || (MyMath = {}));
-var MyMath;
-(function (MyMath) {
-    function CalculateRectangleArea(width, height) {
-        return width * height;
-    }
-    MyMath.CalculateRectangleArea = CalculateRectangleArea;
-})(MyMath || (MyMath = {}));
+"use strict";
 //-------------------------------------------------------------------------------------------------------------------------------------
 //javascript uses module loaders to modularize the js files
 ///NameSpaces and Modules in typescript
@@ -49,16 +29,19 @@ console.log(MyMath.CalculateRectangleArea(20, 40))
 //-------------------------------------------------------------------------------------------------------------------------------------
 // nested namespaces example
 // we also need to use the command ==> tsc NameSpaces.ts --outfile NameSpaces.js here as well
-///<reference path="Circle.ts"/>  //here we need to put the 3 slashes before reference
+/*
+///<reference path="Circle.ts"/>  //here we need to put the 3 slashes before reference // this cannot be commented
 ///<reference path="Rectangle.ts"/>
-console.log(MyMath.MyCircle.CalculateCircumference(20)); //note that here we have used the MyCircle namespace to get to the CalculateCircumference method
-console.log(MyMath.CalculateRectangleArea(20, 40));
+
+console.log(MyMath.MyCircle.CalculateCircumference(20))  //note that here we have used the MyCircle namespace to get to the CalculateCircumference method
+console.log(MyMath.CalculateRectangleArea(20, 40))
+*/
 //-------------------------------------------------------------------------------------------------------------------------------------
 // nested namespaces alias using import example
 // we also need to use the command ==> tsc NameSpaces.ts --outfile NameSpaces.js here as well
 /*
 
-///<reference path="Circle.ts"/>  //here we need to put the 3 slashes before reference
+///<reference path="Circle.ts"/>  //here we need to put the 3 slashes before reference //this cannot be commented
 ///<reference path="Rectangle.ts"/>
 
 import Circle = MyMath.MyCircle;  //this import is different than module import
