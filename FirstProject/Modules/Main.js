bef8d11fd6e50beeb1d9c5492a88e3a14f66e2eb
Nikhil <karanjkarnikhil4@gmail.com>
2018-08-05 14:02:50 +0530

    again more on interfaces

--- FirstProject/Modules/Main.js
+++ FirstProject/Modules/Main.js
@@ -9,8 +9,31 @@
 // Need to module loader
 // Use system js module loader
 // run ==> npm install Systemjs --save  // need to save this for the deployment as well not only for dev dependencies like the lite server
+//-------------------------------------------------------------------------------------------------------------------------------------
+/*
+import {CalculateCircumference,PI} from './CircleModule'  //this is the import syntax used for importing modules the functions or variables inside {curly braces} are imported. Here dont specify the .ts extension
+import {CalculateRectangleArea} from './RectangleModule'
+
+
+console.log(CalculateCircumference(20))
+console.log(CalculateRectangleArea(20, 40))
+*/
+//-------------------------------------------------------------------------------------------------------------------------------------
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var CircleModule_1 = require("./CircleModule"); //this is the import syntax used for importing modules the functions or variables inside {curly braces} are imported. Here dont specify the .ts extension
-var RectangleModule_1 = require("./RectangleModule");
-console.log(CircleModule_1.CalculateCircumference(20));
-console.log(RectangleModule_1.CalculateRectangleArea(20, 40));
+// importing using * and default importing
+var Circle = __importStar(require("./CircleModule")); // here we are importing everything from Circle module and using the alias Circle
+var RectangleModule_1 = __importDefault(require("./RectangleModule")); // here we can specify any name like Rectangle as the default one will be exported here
+console.log(Circle.CalculateCircumference(20));
+console.log(RectangleModule_1.default(20, 40)); // here we use the alias directly to call the function as the default one will be exported here
+console.log(Circle.PI);
+// Also look at NameSpaces_VS_Modules.PNG in this folder for all the differences between Modules and Namespaces
--- FirstProject/Modules/RectangleModule.js
+++ FirstProject/Modules/RectangleModule.js
@@ -1,8 +1,13 @@
 "use strict";
 // here we need to export the function and we dont specify the namespaces
 // modules and namespaces are completely different
+/*
+     export  function CalculateRectangleArea(width: number, height: number): number {
+        return width * height;
+    }
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
 function CalculateRectangleArea(width, height) {
     return width * height;
 }
-exports.CalculateRectangleArea = CalculateRectangleArea;
+exports.default = CalculateRectangleArea;
